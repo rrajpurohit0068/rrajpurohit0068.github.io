4905c30af687d8d7f5b1caddd273640a49829c76
rrajpurohit0068 <43811743+rrajpurohit0068@users.noreply.github.com>
2024-09-15 13:55:58 +0000

    New1

--- souce-code/v3/src/Loader2/index.js
+++ souce-code/v3/src/Loader2/index.js
@@ -3,8 +3,11 @@ import React from 'react';
 import Button from '@mui/material/Button';
 import styled from './loader.module.scss';
 import SendIcon from '@mui/icons-material/Send';
+import { useLocation } from "react-router-dom";
 
 export const Loader = ({ setLoader }) => {
+    const location = useLocation()
+
     React.useEffect(() => {
         const body = document.getElementsByTagName('body')[0];
         body.classList.add('bg-img')
@@ -15,7 +18,7 @@ export const Loader = ({ setLoader }) => {
         }
     }, [])
     return <div  className={styled.button}>
-<Button style={{ background: '#483d8b', color: 'white' }} endIcon={<SendIcon />} href='#/profile' variant="contained">Go to Profile</Button>
+<Button style={{ background: '#483d8b', color: 'white' }} endIcon={<SendIcon />} href={`#/profile${location.search}`} variant="contained">Go to Profile</Button>
     </div> 
     
     
